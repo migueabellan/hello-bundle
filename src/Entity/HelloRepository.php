20d675cf47ce753f4d339ff6e1cb7ce354b9aa3c
Miguel Ángel <migueabellan@gmail.com>
2020-12-29 17:55:21 +0100

    Update

--- src/Entity/HelloRepository.php
+++ src/Entity/HelloRepository.php
@@ -2,15 +2,9 @@
 
 namespace Migueabellan\HelloBundle\Entity;
 
-use Migueabellan\HelloBundle\Entity\Hello;
+use Doctrine\ORM\EntityRepository;
 
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
-
-class HelloRepository extends ServiceEntityRepository
+class HelloRepository extends EntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Hello::class);
-    }
+    
 }
